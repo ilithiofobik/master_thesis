dde611649a8e61f062123d45e873102b814b1aab
Wojciech Sęk <wojciech.sek17@gmail.com>
2024-04-23 18:49:28 +0200

    calc lowpt

--- src/planarity.rs
+++ src/planarity.rs
@@ -4,47 +4,131 @@ use std::collections::HashSet;
 use std::collections::VecDeque;
 use std::rc::Rc;
 use std::sync::RwLock;
+use std::vec;
 
-struct Node {}
-struct Edge {}
+const CCW: usize = 0;
+const CW: usize = 1;
 
-struct AdjElement {
-    twin: Rc<RwLock<AdjElement>>,
-    node: Rc<RwLock<Node>>,
-    edge: Rc<RwLock<Edge>>,
-    id: usize,
+enum AdjacencyListLink {
+    InV(usize),
+    InR(usize),
+    InE(usize),
+}
+
+struct HalfEdge {
+    link: [AdjacencyListLink; 2],
+    neighbour: usize,
+    sign: isize,
+}
+
+struct RootVertex {
+    link: [AdjacencyListLink; 2],
+    parent: usize,
+}
+
+struct Vertex {
+    link: [AdjacencyListLink; 2],
+    dfs_parent: usize,
+    least_ancestor: usize,
+    lowpoint: usize,
+    backedge_flag: usize,
+    pertinent_roots: Vec<usize>,
+    separated_dfs_children: Vec<Rc<RwLock<usize>>>,
+    p_node_in_child_list_of_parent: Rc<RwLock<usize>>,
+}
+
+struct EmbeddingGraph {
+    n: usize,
+    m: usize,
+    v: Vec<Vertex>,
+    r: Vec<RootVertex>,
+    e: Vec<HalfEdge>,
+    s: Vec<usize>,
 }
 
 struct Planarity<'a> {
     graph: &'a Graph,
     n: usize,
+    dfi: Vec<Option<usize>>,
+    lowpoint: Vec<usize>,
 }
 
 impl Planarity<'_> {
     pub fn new(graph: &Graph) -> Planarity {
         let n = graph.num_of_vertices();
-        Planarity { graph, n }
+        let dfi = vec![None; n];
+        let lowpoint = vec![0; n];
+        Planarity {
+            graph,
+            n,
+            dfi,
+            lowpoint,
+        }
     }
 
-    fn compute_dfs(&mut self) {
-        //let mut stack = vec![];
-        let mut next_dfi = 1;
-        let n = self.graph.num_of_vertices();
+    fn biconnect(
+        &mut self,
+        v: usize,
+        u: usize,
+        counter: &mut usize,
+        edge_stack: &mut Vec<(usize, usize)>,
+    ) {
+        *counter += 1;
+        self.dfi[v] = Some(*counter);
+        self.lowpoint[v] = *counter;
 
-        for v in 0..n {}
+        for w in self.graph.neighbours(v).unwrap() {
+            match self.dfi[*w] {
+                None => {
+                    self.biconnect(*w, v, counter, edge_stack);
+                    self.lowpoint[v] = std::cmp::min(self.lowpoint[v], self.lowpoint[*w]);
+                }
+                Some(dfi_w) => {
+                    if dfi_w < self.dfi[v].unwrap() && *w != u {
+                        self.lowpoint[v] = std::cmp::min(self.lowpoint[v], dfi_w);
+                    }
+                }
+            }
+        }
     }
 
-    fn compute_lowpoints(&mut self) {}
+    fn compute_lowpoints(&mut self) {
+        let mut counter = 0;
+        let mut edge_stack = vec![];
+        for w in self.graph.vertices() {
+            if self.dfi[w].is_none() {
+                self.biconnect(w, self.n + 1, &mut counter, &mut edge_stack);
+            }
+        }
+    }
+
+    fn initialize_embedding_graph(&mut self) {}
     fn compute_separated_dfs_children(&mut self) {}
+    fn embed_edge(&mut self, u: usize, v: usize) {}
     fn embed(&mut self) -> bool {
         true
     }
 
+    fn print_lowpoints(&self) {
+        println!("Lowpoints:");
+        for i in 0..self.n {
+            println!("{}: {}", i, self.lowpoint[i]);
+        }
+    }
+
+    fn print_dfi(&self) {
+        println!("DFI:");
+        for i in 0..self.n {
+            println!("{}: {:?}", i, self.dfi[i]);
+        }
+    }
+
     pub fn is_planar(&mut self) -> bool {
-        self.compute_dfs();
         self.compute_lowpoints();
-        self.compute_separated_dfs_children();
-        self.embed()
+        self.print_lowpoints();
+        self.print_dfi();
+
+        true
     }
 }
 
--- src/tests/planarity.rs
+++ src/tests/planarity.rs
@@ -6,3 +6,20 @@ fn k4_test() {
     let graph = Graph::complete(4);
     assert!(is_planar(&graph));
 }
+
+#[test]
+fn tarjan_test() {
+    let mut graph = Graph::empty(9);
+    graph.add_edge(0, 1);
+    graph.add_edge(0, 5);
+    graph.add_edge(0, 6);
+    graph.add_edge(1, 2);
+    graph.add_edge(1, 3);
+    graph.add_edge(1, 4);
+    graph.add_edge(2, 3);
+    graph.add_edge(4, 5);
+    graph.add_edge(6, 8);
+    graph.add_edge(6, 7);
+    graph.add_edge(7, 8);
+    assert!(is_planar(&graph));
+}
