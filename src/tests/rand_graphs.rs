0bdf1ec7b9c0b6bfc645e07b5098fa696fa27600
Wojciech Sęk <wojciech.sek17@gmail.com>
2024-04-29 17:17:25 +0200

    correct bliztstein

--- src/tests/rand_graphs.rs
+++ src/tests/rand_graphs.rs
@@ -1,13 +1,13 @@
 use crate::rand_graphs::bliztstein_generation;
 
-fn blizstein_test(d: &[usize]) {
+fn bliztstein_test(d: &[usize]) {
     let graph = bliztstein_generation(&d).unwrap();
     assert_eq!(graph.num_of_edges(), d.iter().sum::<usize>() / 2);
 
     for u in 0..d.len() {
-        match graph.neighbours(u) {
-            Some(neighbours) => {
-                assert_eq!(neighbours.len(), d[u]);
+        match graph.neighbors(u) {
+            Some(neighbors) => {
+                assert_eq!(neighbors.len(), d[u]);
             }
             None => assert!(false, "Vertex {} not found", u),
         }
@@ -17,7 +17,7 @@ fn blizstein_test(d: &[usize]) {
 #[test]
 fn bliztstein_generation_regular_test() {
     let d = vec![10; 1000];
-    blizstein_test(&d);
+    bliztstein_test(&d);
 }
 
 #[test]
@@ -26,13 +26,13 @@ fn bliztstein_generation_test() {
         7, 8, 5, 1, 1, 2, 8, 10, 4, 2, 4, 5, 3, 6, 7, 3, 2, 7, 6, 1, 2, 9, 6, 1, 3, 4, 6, 3, 3, 3,
         2, 4, 4,
     ];
-    blizstein_test(&d);
+    bliztstein_test(&d);
 }
 
 #[test]
 fn bliztstein_generation_complete_test() {
     let d = vec![99; 100];
-    blizstein_test(&d);
+    bliztstein_test(&d);
 }
 
 #[test]
